d66090a2178bb8cf74b5a90dcc84547ad1dcbfa6
Dogac Akyıldız <ndakyildiz@gmail.com>
2020-08-20 01:16:50 +0300

    Add KafkaConsumer.

---
+++ KafkaSamples/KafkaConsumer.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace KafkaSamples
+{
+    public class KafkaConsumer
+    {
+        public static async Task<Message<TKey, TValue>> ListenAsync<TKey, TValue>(
+            string bootstrapServers, 
+            CancellationToken token = default)
+        {
+            ConsumerConfig config = new ConsumerConfig
+            {
+                BootstrapServers = bootstrapServers,
+                EnableAutoCommit = true
+            };
+
+            var builder = new ConsumerBuilder<TKey, TValue>(config);
+
+            using var consumer = builder.Build();
+
+            while (true)
+            {
+                var consumeResult = consumer.Consume(token);
+
+                if (consumeResult.IsPartitionEOF)
+                    return default;
+
+                return consumeResult.Message;
+            }
+        }
+    }
+}
