aa96349ca37cfffb0396242f706915aeb07e6484
Dogac Akyıldız <ndakyildiz@gmail.com>
2020-08-26 18:43:38 +0300

    Add Producer.

--- KafkaSamples/Producer/IKafkaProducer.cs
+++ KafkaSamples/Producer/IKafkaProducer.cs
@@ -1,7 +1,12 @@
-﻿namespace KafkaSamples.Producer
-{
-    public interface IKafkaProducer
-    {
+﻿using Confluent.Kafka;
+using System.Threading.Tasks;
 
+namespace KafkaSamples.Producer
+{
+    public interface IKafkaProducer<TKey, TValue>
+    {
+        Task ProduceAsync(string topic, TKey key, TValue value);
+
+        Task ProduceAsync(TopicPartition tp, TKey key, TValue value);
     }
 }
---
+++ KafkaSamples/Producer/KafkaProducer.cs
@@ -0,0 +1,37 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading.Tasks;
+
+namespace KafkaSamples.Producer
+{
+    public class KafkaProducer<TKey, TValue> : IKafkaProducer<TKey, TValue>, IDisposable
+    {
+        private readonly IProducer<TKey, TValue> _producer;
+
+        public KafkaProducer()
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "my-servers",
+                RequestTimeoutMs = 5000
+            };
+
+            _producer = new ProducerBuilder<TKey, TValue>(config).Build();
+        }
+
+        public void Dispose()
+        {
+            _producer?.Dispose();
+        }
+
+        public async Task ProduceAsync(string topic, TKey key, TValue value)
+        {
+            await ProduceAsync(new TopicPartition(topic, Partition.Any), key, value);
+        }
+
+        public async Task ProduceAsync(TopicPartition tp, TKey key, TValue value)
+        {
+            await _producer.ProduceAsync(tp, new Message<TKey, TValue>());
+        }
+    }
+}
